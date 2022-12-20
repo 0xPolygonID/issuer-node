70262135d482434688421a86090553ba969b2b8c
Martin Saporiti <martinsaporiti@gmail.com>
2022-12-20 14:40:23 -0300

    feat: add viper

---
+++ internal/config/config.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+	"flag"
+	"fmt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Configuration struct {
+	ServerPort string `mapstructure:"ServerPort"`
+}
+
+func Load(path string) (*Configuration, error) {
+	getFlags()
+	bindEnv()
+	pathFlag := viper.GetString("config")
+	if _, err := os.Stat(pathFlag); err == nil {
+		ext := filepath.Ext(pathFlag)
+		if len(ext) > 1 {
+			ext = ext[1:]
+		}
+		name := strings.Split(filepath.Base(pathFlag), ".")[0]
+		viper.AddConfigPath(".")
+		viper.SetConfigName(name)
+		viper.SetConfigType(ext)
+	} else {
+		// Read default config file.
+		viper.AddConfigPath(path)
+		viper.SetConfigName("config")
+		viper.SetConfigType("toml")
+	}
+
+	config := new(Configuration)
+
+	if err := viper.ReadInConfig(); err == nil {
+		if err := viper.Unmarshal(config); err != nil {
+			return nil, err
+		}
+	} else {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func getFlags() {
+	pflag.StringP("config", "c", "", "Specify the configuration file location.")
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+
+	if err := viper.BindPFlags(pflag.CommandLine); err != nil {
+		fmt.Printf("Error parsing flags: %s", err.Error())
+	}
+}
+
+func bindEnv() {
+	viper.SetEnvPrefix("SH_ID_PLATFORM")
+	_ = viper.BindEnv("ServerPort", "SH_ID_PLATFORM_SERVER_PORT")
+	viper.AutomaticEnv()
+}
