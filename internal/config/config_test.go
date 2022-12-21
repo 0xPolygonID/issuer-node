30a460f555a7fed0b734af3e1e357524a147edfb
Martin Saporiti <martinsaporiti@gmail.com>
2022-12-21 06:35:06 -0300

    feat: add viper (#13)
    * add mod file
    
    * feat: add viper
    
    * feat: improve paths
    
    * annotation removed

---
+++ internal/config/config_test.go
@@ -0,0 +1,13 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfig(t *testing.T) {
+	configuration, err := Load(".")
+	assert.NoError(t, err)
+	assert.Equal(t, "3001", configuration.ServerPort)
+}
