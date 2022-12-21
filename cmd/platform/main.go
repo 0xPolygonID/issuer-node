42947abcac49f61a8d655a6a5e50fa727c983b37
x1m3 <jgimenez@polygon.technology>
2022-12-21 18:36:55 +0100

    chore: Use port number from config

--- cmd/platform/main.go
+++ cmd/platform/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
@@ -9,15 +10,20 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
 	"github.com/polygonid/polygonid-api/sh-id-platform/internal/api"
+	"github.com/polygonid/polygonid-api/sh-id-platform/internal/config"
 )
 
 func main() {
+	cfg, err := config.Load("internal/config")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	mux := echo.New()
 	api.RegisterHandlers(mux, api.NewStrictHandler(&api.Server{}, nil))
 
-	addr := ":3000"
 	server := &http.Server{
-		Addr:    addr,
+		Addr:    fmt.Sprintf(":%d", cfg.ServerPort),
 		Handler: mux,
 	}
 	quit := make(chan os.Signal, 1)
