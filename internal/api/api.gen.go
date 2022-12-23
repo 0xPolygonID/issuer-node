41d3737838b86c80784506332bd4d2c310557871
Javi <javierperezies@gmail.com>
2022-12-23 14:39:59 +0100

    feat: adding identity and claims openapi endpoints with models (#38)
    * feat: adding identity and claims endpoints openapi with models
    
    * feat: adding open api
    
    * feat: adding open api models with chi

--- internal/api/api.gen.go
+++ internal/api/api.gen.go
@@ -8,9 +8,52 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/deepmap/oapi-codegen/pkg/runtime"
 	"github.com/go-chi/chi/v5"
+	core "github.com/iden3/go-iden3-core"
+	"github.com/iden3/go-merkletree-sql"
 )
+
+// CreateClaimRequest defines model for CreateClaimRequest.
+type CreateClaimRequest struct {
+	CredentialSchema      string          `json:"credentialSchema"`
+	CredentialSubject     json.RawMessage `json:"credentialSubject"`
+	Expiration            int64           `json:"expiration"`
+	MerklizedRootPosition string          `json:"merklizedRootPosition"`
+	RevNonce              *uint64         `json:"revNonce,omitempty"`
+	SubjectPosition       string          `json:"subjectPosition"`
+	Type                  string          `json:"type"`
+	Version               uint32          `json:"version"`
+}
+
+// CreateClaimResponse defines model for CreateClaimResponse.
+type CreateClaimResponse struct {
+	Id string `json:"id"`
+}
+
+// CreateIdentityResponse defines model for CreateIdentityResponse.
+type CreateIdentityResponse struct {
+	Identifier *string `json:"identifier,omitempty"`
+	Immutable  bool    `json:"immutable"`
+	Relay      string  `json:"relay"`
+	State      *struct {
+		BlockNumber        *int      `json:"blockNumber,omitempty"`
+		BlockTimestamp     *int      `json:"blockTimestamp,omitempty"`
+		ClaimsTreeRoot     *string   `json:"claimsTreeRoot,omitempty"`
+		CreatedAt          time.Time `json:"createdAt"`
+		Identifier         string    `json:"-"`
+		ModifiedAt         time.Time `json:"modifiedAt"`
+		PreviousState      *string   `json:"previousState,omitempty"`
+		RevocationTreeRoot *string   `json:"revocationTreeRoot,omitempty"`
+		RootOfRoots        *string   `json:"rootOfRoots,omitempty"`
+		State              *string   `json:"state,omitempty"`
+		StateID            int64     `json:"-"`
+		Status             string    `json:"status"`
+		TxID               *string   `json:"txID,omitempty"`
+	} `json:"state,omitempty"`
+}
 
 // GenericErrorMessage defines model for GenericErrorMessage.
 type GenericErrorMessage struct {
@@ -28,6 +71,34 @@ type Pong struct {
 	Response *string `json:"response,omitempty"`
 }
 
+// RevocationStatusResponse defines model for RevocationStatusResponse.
+type RevocationStatusResponse struct {
+	Issuer struct {
+		ClaimsTreeRoot     *string `json:"claimsTreeRoot,omitempty"`
+		RevocationTreeRoot *string `json:"revocationTreeRoot,omitempty"`
+		RootOfRoots        *string `json:"rootOfRoots,omitempty"`
+		State              *string `json:"state,omitempty"`
+	} `json:"issuer"`
+	Mtp struct {
+		Existence bool `json:"existence"`
+		NodeAux   *struct {
+			Key   *merkletree.Hash `json:"key,omitempty"`
+			Value *merkletree.Hash `json:"value,omitempty"`
+		} `json:"nodeAux,omitempty"`
+	} `json:"mtp"`
+}
+
+// RevokeClaimResponse defines model for RevokeClaimResponse.
+type RevokeClaimResponse struct {
+	Status string `json:"status"`
+}
+
+// PathIdentifier defines model for pathIdentifier.
+type PathIdentifier = core.DID
+
+// PathNonce defines model for pathNonce.
+type PathNonce = int64
+
 // N400 defines model for 400.
 type N400 = GenericErrorMessage
 
@@ -43,6 +114,16 @@ type N407 = GenericErrorMessage
 // N500 defines model for 500.
 type N500 = GenericErrorMessage
 
+// N500CreateIdentity defines model for 500-CreateIdentity.
+type N500CreateIdentity struct {
+	Code      *int    `json:"code,omitempty"`
+	Error     *string `json:"error,omitempty"`
+	RequestID *string `json:"requestID,omitempty"`
+}
+
+// CreateClaimJSONRequestBody defines body for CreateClaim for application/json ContentType.
+type CreateClaimJSONRequestBody = CreateClaimRequest
+
 // ServerInterface represents all server handlers.
 type ServerInterface interface {
 	// Play Ping Pong
@@ -54,6 +135,18 @@ type ServerInterface interface {
 	// Healthcheck
 	// (GET /status)
 	Health(w http.ResponseWriter, r *http.Request)
+	// Create Identity
+	// (POST /v1/identities)
+	CreateIdentity(w http.ResponseWriter, r *http.Request)
+	// Create Claim
+	// (POST /v1/{identifier}/claims)
+	CreateClaim(w http.ResponseWriter, r *http.Request, identifier PathIdentifier)
+	// Get Revocation Status
+	// (GET /v1/{identifier}/claims/revocation/status/{nonce})
+	GetRevocationStatus(w http.ResponseWriter, r *http.Request, identifier PathIdentifier, nonce PathNonce)
+	// Revoke Claim
+	// (POST /v1/{identifier}/claims/revoke/{nonce})
+	RevokeClaim(w http.ResponseWriter, r *http.Request, identifier PathIdentifier, nonce PathNonce)
 }
 
 // ServerInterfaceWrapper converts contexts to parameters.
@@ -101,6 +194,117 @@ func (siw *ServerInterfaceWrapper) Health(w http.ResponseWriter, r *http.Request
 
 	var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		siw.Handler.Health(w, r)
+	})
+
+	for _, middleware := range siw.HandlerMiddlewares {
+		handler = middleware(handler)
+	}
+
+	handler.ServeHTTP(w, r.WithContext(ctx))
+}
+
+// CreateIdentity operation middleware
+func (siw *ServerInterfaceWrapper) CreateIdentity(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		siw.Handler.CreateIdentity(w, r)
+	})
+
+	for _, middleware := range siw.HandlerMiddlewares {
+		handler = middleware(handler)
+	}
+
+	handler.ServeHTTP(w, r.WithContext(ctx))
+}
+
+// CreateClaim operation middleware
+func (siw *ServerInterfaceWrapper) CreateClaim(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var err error
+
+	// ------------- Path parameter "identifier" -------------
+	var identifier PathIdentifier
+
+	err = runtime.BindStyledParameterWithLocation("simple", false, "identifier", runtime.ParamLocationPath, chi.URLParam(r, "identifier"), &identifier)
+	if err != nil {
+		siw.ErrorHandlerFunc(w, r, &InvalidParamFormatError{ParamName: "identifier", Err: err})
+		return
+	}
+
+	var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		siw.Handler.CreateClaim(w, r, identifier)
+	})
+
+	for _, middleware := range siw.HandlerMiddlewares {
+		handler = middleware(handler)
+	}
+
+	handler.ServeHTTP(w, r.WithContext(ctx))
+}
+
+// GetRevocationStatus operation middleware
+func (siw *ServerInterfaceWrapper) GetRevocationStatus(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var err error
+
+	// ------------- Path parameter "identifier" -------------
+	var identifier PathIdentifier
+
+	err = runtime.BindStyledParameterWithLocation("simple", false, "identifier", runtime.ParamLocationPath, chi.URLParam(r, "identifier"), &identifier)
+	if err != nil {
+		siw.ErrorHandlerFunc(w, r, &InvalidParamFormatError{ParamName: "identifier", Err: err})
+		return
+	}
+
+	// ------------- Path parameter "nonce" -------------
+	var nonce PathNonce
+
+	err = runtime.BindStyledParameterWithLocation("simple", false, "nonce", runtime.ParamLocationPath, chi.URLParam(r, "nonce"), &nonce)
+	if err != nil {
+		siw.ErrorHandlerFunc(w, r, &InvalidParamFormatError{ParamName: "nonce", Err: err})
+		return
+	}
+
+	var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		siw.Handler.GetRevocationStatus(w, r, identifier, nonce)
+	})
+
+	for _, middleware := range siw.HandlerMiddlewares {
+		handler = middleware(handler)
+	}
+
+	handler.ServeHTTP(w, r.WithContext(ctx))
+}
+
+// RevokeClaim operation middleware
+func (siw *ServerInterfaceWrapper) RevokeClaim(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var err error
+
+	// ------------- Path parameter "identifier" -------------
+	var identifier PathIdentifier
+
+	err = runtime.BindStyledParameterWithLocation("simple", false, "identifier", runtime.ParamLocationPath, chi.URLParam(r, "identifier"), &identifier)
+	if err != nil {
+		siw.ErrorHandlerFunc(w, r, &InvalidParamFormatError{ParamName: "identifier", Err: err})
+		return
+	}
+
+	// ------------- Path parameter "nonce" -------------
+	var nonce PathNonce
+
+	err = runtime.BindStyledParameterWithLocation("simple", false, "nonce", runtime.ParamLocationPath, chi.URLParam(r, "nonce"), &nonce)
+	if err != nil {
+		siw.ErrorHandlerFunc(w, r, &InvalidParamFormatError{ParamName: "nonce", Err: err})
+		return
+	}
+
+	var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		siw.Handler.RevokeClaim(w, r, identifier, nonce)
 	})
 
 	for _, middleware := range siw.HandlerMiddlewares {
@@ -232,6 +436,18 @@ func HandlerWithOptions(si ServerInterface, options ChiServerOptions) http.Handl
 	r.Group(func(r chi.Router) {
 		r.Get(options.BaseURL+"/status", wrapper.Health)
 	})
+	r.Group(func(r chi.Router) {
+		r.Post(options.BaseURL+"/v1/identities", wrapper.CreateIdentity)
+	})
+	r.Group(func(r chi.Router) {
+		r.Post(options.BaseURL+"/v1/{identifier}/claims", wrapper.CreateClaim)
+	})
+	r.Group(func(r chi.Router) {
+		r.Get(options.BaseURL+"/v1/{identifier}/claims/revocation/status/{nonce}", wrapper.GetRevocationStatus)
+	})
+	r.Group(func(r chi.Router) {
+		r.Post(options.BaseURL+"/v1/{identifier}/claims/revoke/{nonce}", wrapper.RevokeClaim)
+	})
 
 	return r
 }
@@ -245,6 +461,12 @@ type N402JSONResponse GenericErrorMessage
 type N407JSONResponse GenericErrorMessage
 
 type N500JSONResponse GenericErrorMessage
+
+type N500CreateIdentityJSONResponse struct {
+	Code      *int    `json:"code,omitempty"`
+	Error     *string `json:"error,omitempty"`
+	RequestID *string `json:"requestID,omitempty"`
+}
 
 type PingRequestObject struct {
 }
@@ -348,6 +570,139 @@ func (response Health500JSONResponse) VisitHealthResponse(w http.ResponseWriter)
 	return json.NewEncoder(w).Encode(response)
 }
 
+type CreateIdentityRequestObject struct {
+}
+
+type CreateIdentityResponseObject interface {
+	VisitCreateIdentityResponse(w http.ResponseWriter) error
+}
+
+type CreateIdentity201JSONResponse CreateIdentityResponse
+
+func (response CreateIdentity201JSONResponse) VisitCreateIdentityResponse(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+
+	return json.NewEncoder(w).Encode(response)
+}
+
+type CreateIdentity500JSONResponse struct{ N500CreateIdentityJSONResponse }
+
+func (response CreateIdentity500JSONResponse) VisitCreateIdentityResponse(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(500)
+
+	return json.NewEncoder(w).Encode(response)
+}
+
+type CreateClaimRequestObject struct {
+	Identifier PathIdentifier `json:"identifier"`
+	Body       *CreateClaimJSONRequestBody
+}
+
+type CreateClaimResponseObject interface {
+	VisitCreateClaimResponse(w http.ResponseWriter) error
+}
+
+type CreateClaim201JSONResponse CreateClaimResponse
+
+func (response CreateClaim201JSONResponse) VisitCreateClaimResponse(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+
+	return json.NewEncoder(w).Encode(response)
+}
+
+type CreateClaim400JSONResponse struct{ N400JSONResponse }
+
+func (response CreateClaim400JSONResponse) VisitCreateClaimResponse(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(400)
+
+	return json.NewEncoder(w).Encode(response)
+}
+
+type CreateClaim500JSONResponse struct{ N500JSONResponse }
+
+func (response CreateClaim500JSONResponse) VisitCreateClaimResponse(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(500)
+
+	return json.NewEncoder(w).Encode(response)
+}
+
+type GetRevocationStatusRequestObject struct {
+	Identifier PathIdentifier `json:"identifier"`
+	Nonce      PathNonce      `json:"nonce"`
+}
+
+type GetRevocationStatusResponseObject interface {
+	VisitGetRevocationStatusResponse(w http.ResponseWriter) error
+}
+
+type GetRevocationStatus200JSONResponse RevocationStatusResponse
+
+func (response GetRevocationStatus200JSONResponse) VisitGetRevocationStatusResponse(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+
+	return json.NewEncoder(w).Encode(response)
+}
+
+type GetRevocationStatus400JSONResponse struct{ N400JSONResponse }
+
+func (response GetRevocationStatus400JSONResponse) VisitGetRevocationStatusResponse(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(400)
+
+	return json.NewEncoder(w).Encode(response)
+}
+
+type GetRevocationStatus500JSONResponse struct{ N500JSONResponse }
+
+func (response GetRevocationStatus500JSONResponse) VisitGetRevocationStatusResponse(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(500)
+
+	return json.NewEncoder(w).Encode(response)
+}
+
+type RevokeClaimRequestObject struct {
+	Identifier PathIdentifier `json:"identifier"`
+	Nonce      PathNonce      `json:"nonce"`
+}
+
+type RevokeClaimResponseObject interface {
+	VisitRevokeClaimResponse(w http.ResponseWriter) error
+}
+
+type RevokeClaim202JSONResponse RevokeClaimResponse
+
+func (response RevokeClaim202JSONResponse) VisitRevokeClaimResponse(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(202)
+
+	return json.NewEncoder(w).Encode(response)
+}
+
+type RevokeClaim400JSONResponse struct{ N400JSONResponse }
+
+func (response RevokeClaim400JSONResponse) VisitRevokeClaimResponse(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(400)
+
+	return json.NewEncoder(w).Encode(response)
+}
+
+type RevokeClaim500JSONResponse struct{ N500JSONResponse }
+
+func (response RevokeClaim500JSONResponse) VisitRevokeClaimResponse(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(500)
+
+	return json.NewEncoder(w).Encode(response)
+}
+
 // StrictServerInterface represents all server handlers.
 type StrictServerInterface interface {
 	// Play Ping Pong
@@ -359,6 +714,18 @@ type StrictServerInterface interface {
 	// Healthcheck
 	// (GET /status)
 	Health(ctx context.Context, request HealthRequestObject) (HealthResponseObject, error)
+	// Create Identity
+	// (POST /v1/identities)
+	CreateIdentity(ctx context.Context, request CreateIdentityRequestObject) (CreateIdentityResponseObject, error)
+	// Create Claim
+	// (POST /v1/{identifier}/claims)
+	CreateClaim(ctx context.Context, request CreateClaimRequestObject) (CreateClaimResponseObject, error)
+	// Get Revocation Status
+	// (GET /v1/{identifier}/claims/revocation/status/{nonce})
+	GetRevocationStatus(ctx context.Context, request GetRevocationStatusRequestObject) (GetRevocationStatusResponseObject, error)
+	// Revoke Claim
+	// (POST /v1/{identifier}/claims/revoke/{nonce})
+	RevokeClaim(ctx context.Context, request RevokeClaimRequestObject) (RevokeClaimResponseObject, error)
 }
 
 type StrictHandlerFunc func(ctx context.Context, w http.ResponseWriter, r *http.Request, args interface{}) (interface{}, error)
@@ -456,6 +823,117 @@ func (sh *strictHandler) Health(w http.ResponseWriter, r *http.Request) {
 		sh.options.ResponseErrorHandlerFunc(w, r, err)
 	} else if validResponse, ok := response.(HealthResponseObject); ok {
 		if err := validResponse.VisitHealthResponse(w); err != nil {
+			sh.options.ResponseErrorHandlerFunc(w, r, err)
+		}
+	} else if response != nil {
+		sh.options.ResponseErrorHandlerFunc(w, r, fmt.Errorf("Unexpected response type: %T", response))
+	}
+}
+
+// CreateIdentity operation middleware
+func (sh *strictHandler) CreateIdentity(w http.ResponseWriter, r *http.Request) {
+	var request CreateIdentityRequestObject
+
+	handler := func(ctx context.Context, w http.ResponseWriter, r *http.Request, request interface{}) (interface{}, error) {
+		return sh.ssi.CreateIdentity(ctx, request.(CreateIdentityRequestObject))
+	}
+	for _, middleware := range sh.middlewares {
+		handler = middleware(handler, "CreateIdentity")
+	}
+
+	response, err := handler(r.Context(), w, r, request)
+
+	if err != nil {
+		sh.options.ResponseErrorHandlerFunc(w, r, err)
+	} else if validResponse, ok := response.(CreateIdentityResponseObject); ok {
+		if err := validResponse.VisitCreateIdentityResponse(w); err != nil {
+			sh.options.ResponseErrorHandlerFunc(w, r, err)
+		}
+	} else if response != nil {
+		sh.options.ResponseErrorHandlerFunc(w, r, fmt.Errorf("Unexpected response type: %T", response))
+	}
+}
+
+// CreateClaim operation middleware
+func (sh *strictHandler) CreateClaim(w http.ResponseWriter, r *http.Request, identifier PathIdentifier) {
+	var request CreateClaimRequestObject
+
+	request.Identifier = identifier
+
+	var body CreateClaimJSONRequestBody
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		sh.options.RequestErrorHandlerFunc(w, r, fmt.Errorf("can't decode JSON body: %w", err))
+		return
+	}
+	request.Body = &body
+
+	handler := func(ctx context.Context, w http.ResponseWriter, r *http.Request, request interface{}) (interface{}, error) {
+		return sh.ssi.CreateClaim(ctx, request.(CreateClaimRequestObject))
+	}
+	for _, middleware := range sh.middlewares {
+		handler = middleware(handler, "CreateClaim")
+	}
+
+	response, err := handler(r.Context(), w, r, request)
+
+	if err != nil {
+		sh.options.ResponseErrorHandlerFunc(w, r, err)
+	} else if validResponse, ok := response.(CreateClaimResponseObject); ok {
+		if err := validResponse.VisitCreateClaimResponse(w); err != nil {
+			sh.options.ResponseErrorHandlerFunc(w, r, err)
+		}
+	} else if response != nil {
+		sh.options.ResponseErrorHandlerFunc(w, r, fmt.Errorf("Unexpected response type: %T", response))
+	}
+}
+
+// GetRevocationStatus operation middleware
+func (sh *strictHandler) GetRevocationStatus(w http.ResponseWriter, r *http.Request, identifier PathIdentifier, nonce PathNonce) {
+	var request GetRevocationStatusRequestObject
+
+	request.Identifier = identifier
+	request.Nonce = nonce
+
+	handler := func(ctx context.Context, w http.ResponseWriter, r *http.Request, request interface{}) (interface{}, error) {
+		return sh.ssi.GetRevocationStatus(ctx, request.(GetRevocationStatusRequestObject))
+	}
+	for _, middleware := range sh.middlewares {
+		handler = middleware(handler, "GetRevocationStatus")
+	}
+
+	response, err := handler(r.Context(), w, r, request)
+
+	if err != nil {
+		sh.options.ResponseErrorHandlerFunc(w, r, err)
+	} else if validResponse, ok := response.(GetRevocationStatusResponseObject); ok {
+		if err := validResponse.VisitGetRevocationStatusResponse(w); err != nil {
+			sh.options.ResponseErrorHandlerFunc(w, r, err)
+		}
+	} else if response != nil {
+		sh.options.ResponseErrorHandlerFunc(w, r, fmt.Errorf("Unexpected response type: %T", response))
+	}
+}
+
+// RevokeClaim operation middleware
+func (sh *strictHandler) RevokeClaim(w http.ResponseWriter, r *http.Request, identifier PathIdentifier, nonce PathNonce) {
+	var request RevokeClaimRequestObject
+
+	request.Identifier = identifier
+	request.Nonce = nonce
+
+	handler := func(ctx context.Context, w http.ResponseWriter, r *http.Request, request interface{}) (interface{}, error) {
+		return sh.ssi.RevokeClaim(ctx, request.(RevokeClaimRequestObject))
+	}
+	for _, middleware := range sh.middlewares {
+		handler = middleware(handler, "RevokeClaim")
+	}
+
+	response, err := handler(r.Context(), w, r, request)
+
+	if err != nil {
+		sh.options.ResponseErrorHandlerFunc(w, r, err)
+	} else if validResponse, ok := response.(RevokeClaimResponseObject); ok {
+		if err := validResponse.VisitRevokeClaimResponse(w); err != nil {
 			sh.options.ResponseErrorHandlerFunc(w, r, err)
 		}
 	} else if response != nil {
