2ddc3a91e5acc82bb8ce8d2dbf63d77d0a1544a8
Martin Saporiti <martinsaporiti@gmail.com>
2023-01-10 14:53:13 -0300

    chore: improv

--- internal/db/tests/fixture.go
+++ internal/db/tests/fixture.go
@@ -4,47 +4,12 @@ import (
 	"context"
 	"testing"
 
-	"github.com/polygonid/sh-id-platform/internal/db"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/polygonid/sh-id-platform/internal/core/ports"
+	"github.com/polygonid/sh-id-platform/internal/db"
 	"github.com/polygonid/sh-id-platform/internal/repositories"
 )
-
-var queries = []string{
-	`
-INSERT INTO claims
-  (
-    id, identifier, issuer, schema_hash, schema_url, schema_type,
-    other_identifier, expiration, version, rev_nonce, core_claim,
-    identity_state
-  )
-VALUES
-  (
-    'bae98b8d-e32e-4990-9e97-1f2fda420914',
-    'did:iden3:polygon:mumbai:wyFiV4w71QgWPn6bYLsZoysFay66gKtVa9kfu6yMZ',
-    'did:iden3:polygon:mumbai:wyFiV4w71QgWPn6bYLsZoysFay66gKtVa9kfu6yMZ',
-    'ca938857241db9451ea329256b9c06e5',
-    'https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/auth.json-ld',
-    'AuthBJJCredential', '', 0, 0, 0, '["0","0","0","0","0","0","0","0"]',
-    NULL
-  ),
-  (
-    '7d78b799-03ea-4b34-8703-b4c677ee0326',
-    'did:iden3:polygon:mumbai:x2Uw18ATvY7mEsgfrrDipBmQQdPWAao4NmF56wGvp',
-    'did:iden3:polygon:mumbai:x2Uw18ATvY7mEsgfrrDipBmQQdPWAao4NmF56wGvp',
-    'ca938857241db9451ea329256b9c06e5',
-    'https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/auth.json-ld',
-    'AuthBJJCredential', '', 0, 0, 0, '["0","0","0","0","0","0","0","0"]',
-    '6313885808cc4d87c87bbeff937376646b4a6cce024e6af598cad7cf8d50be25'
-  )
-`,
-	`
-INSERT INTO identity_states (state, status)
-VALUES
-  ('6313885808cc4d87c87bbeff937376646b4a6cce024e6af598cad7cf8d50be25', 'confirmed')
-`,
-}
 
 type Fixture struct {
 	storage            *db.Storage
