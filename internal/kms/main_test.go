1f2bb0fc785390352eb81201fc9a80d51083ba85
Martin Saporiti <martinsaporiti@gmail.com>
2023-01-18 06:30:44 -0300

    fix: config file

--- internal/kms/main_test.go
+++ internal/kms/main_test.go
@@ -28,12 +28,6 @@ func TestMain(m *testing.M) {
 }
 
 func testMain(m *testing.M) int {
-	//cfg = config.KeyStore{
-	//	Address:              "http://localhost:8200",
-	//	Token:                "hvs.YxU2dLZljGpqLyPYu6VeYJde",
-	//	PluginIden3MountPath: "iden3",
-	//}
-
 	configForTesting, err := config.Load("")
 	if err != nil {
 		log.Error(context.Background(), "cannot load config", err)
