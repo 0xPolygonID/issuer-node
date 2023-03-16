7a73cd717b0de6b2be6a076db9443836c1678d61
Chris N <chris@polygon.technology>
2023-03-16 15:57:58 +0100

    Simplified kebabCase.

--- ui/src/utils/string.ts
+++ ui/src/utils/string.ts
@@ -1,7 +1,7 @@
 export function kebabCase(string: string) {
-  const kebab = string.match(
-    /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
-  ) || ["-"];
-
-  return kebab.join("-").toLowerCase();
+  return (
+    string.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g) || ["-"]
+  )
+    .join("-")
+    .toLowerCase();
 }
