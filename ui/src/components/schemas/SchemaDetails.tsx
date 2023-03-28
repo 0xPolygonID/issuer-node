a2a00904d7038902c22ff3b3b73811fd70dcc081
Chris N <cneuro@users.noreply.github.com>
2023-03-28 12:07:12 +0200

    DRYing up schema details & preview. (#262)
    * Added SchemaViewer & JSONHighlighter, integrated with schema preview & details.
    
    * Reverted test schema change.
    
    * Removed unneeded checks.
    
    * Improved view DOM.
    
    * Avoid unknown error and simplify state (#264)
    
    * Avoid unknown error and simplify state
    
    * Rename getJsonLdTypesFromUrl as getSchemaJsonLdTypes
    
    * Added card centered class
    
    ---------
    
    Co-authored-by: Chris N <chris@polygon.technology>
    
    ---------
    
    Co-authored-by: amonsosanz <amonsosanz@gmail.com>

--- ui/src/components/schemas/SchemaDetails.tsx
+++ ui/src/components/schemas/SchemaDetails.tsx
@@ -1,84 +1,67 @@
-import { Button, Card, Dropdown, Row, Space, Typography, message } from "antd";
+import { Button, Card, Row, Space, Typography, message } from "antd";
 import { useCallback, useEffect, useState } from "react";
 import { generatePath, useNavigate, useParams } from "react-router-dom";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { a11yLight } from "react-syntax-highlighter/dist/esm/styles/hljs";
-
 import { z } from "zod";
-import { ErrorResult } from "../shared/ErrorResult";
-import { LoadingResult } from "../shared/LoadingResult";
-import { SiderLayoutContent } from "../shared/SiderLayoutContent";
+
 import { Schema as ApiSchema, getSchema } from "src/adapters/api/schemas";
-import { ReactComponent as ChevronDownIcon } from "src/assets/icons/chevron-down.svg";
 import { ReactComponent as CreditCardIcon } from "src/assets/icons/credit-card-plus.svg";
 import { Detail } from "src/components/schemas/Detail";
-import { SchemaTree } from "src/components/schemas/SchemaTree";
+import { SchemaViewer } from "src/components/schemas/SchemaViewer";
+import { ErrorResult } from "src/components/shared/ErrorResult";
+import { LoadingResult } from "src/components/shared/LoadingResult";
+import { SiderLayoutContent } from "src/components/shared/SiderLayoutContent";
 import { useEnvContext } from "src/contexts/env";
 import { Json, JsonLdType, Schema } from "src/domain";
 import { ROUTES } from "src/routes";
 import {
   APIError,
   downloadJsonFromUrl,
-  getJsonLdTypesFromUrl,
   getSchemaFromUrl,
+  getSchemaJsonLdTypes,
   processZodError,
 } from "src/utils/adapters";
 import { isAbortedError, makeRequestAbortable } from "src/utils/browser";
-import { CARD_WIDTH } from "src/utils/constants";
 import { formatDate } from "src/utils/forms";
 import { AsyncTask, hasAsyncTaskFailed, isAsyncTaskStarting } from "src/utils/types";
 
-type JsonView = "formatted" | "jsonLdContext" | "jsonSchema";
-
-const JSON_VIEW_LABELS: Record<JsonView, string> = {
-  formatted: "Formatted",
-  jsonLdContext: "JSON LD Context",
-  jsonSchema: "JSON Schema",
-};
-
 export function SchemaDetails() {
-  const env = useEnvContext();
-
   const navigate = useNavigate();
-
   const { schemaID } = useParams();
 
-  const [jsonView, setJsonView] = useState<JsonView>("formatted");
-  const [schema, setSchema] = useState<AsyncTask<Schema, string | z.ZodError>>({
+  const env = useEnvContext();
+
+  const [schemaTuple, setSchemaTuple] = useState<AsyncTask<[Schema, Json], string | z.ZodError>>({
     status: "pending",
   });
   const [apiSchema, setApiSchema] = useState<AsyncTask<ApiSchema, APIError>>({
     status: "pending",
   });
-  const [rawJsonSchema, setRawJsonSchema] = useState<Json | undefined>();
-  const [jsonLdType, setJsonLdType] = useState<AsyncTask<JsonLdType, string | z.ZodError>>({
+  const [contextTuple, setContextTuple] = useState<
+    AsyncTask<[JsonLdType, Json], string | z.ZodError>
+  >({
     status: "pending",
   });
-  const [rawJsonLdContext, setRawJsonLdContext] = useState<Json | undefined>();
 
   const extractError = (error: unknown) =>
     error instanceof z.ZodError ? error : error instanceof Error ? error.message : "Unknown error";
 
   const fetchSchemaFromUrl = useCallback((apiSchema: ApiSchema): void => {
-    setSchema({ status: "loading" });
+    setSchemaTuple({ status: "loading" });
     getSchemaFromUrl({
       url: apiSchema.url,
     })
-      .then(([schema, rawSchema]) => {
-        setSchema({ data: schema, status: "successful" });
-        setRawJsonSchema(rawSchema);
-        setJsonLdType({ status: "loading" });
-        getJsonLdTypesFromUrl({
-          schema: schema,
-          url: schema.$metadata.uris.jsonLdContext,
+      .then(([schema, rawJsonSchema]) => {
+        setSchemaTuple({ data: [schema, rawJsonSchema], status: "successful" });
+        setContextTuple({ status: "loading" });
+        getSchemaJsonLdTypes({
+          schema,
         })
           .then(([jsonLdTypes, rawJsonLdContext]) => {
-            setRawJsonLdContext(rawJsonLdContext);
             const jsonLdType = jsonLdTypes.find((type) => type.name === apiSchema.type);
             if (jsonLdType) {
-              setJsonLdType({ data: jsonLdType, status: "successful" });
+              setContextTuple({ data: [jsonLdType, rawJsonLdContext], status: "successful" });
             } else {
-              setJsonLdType({
+              setContextTuple({
                 error:
                   "Couldn't find the type specified by the schemas API in the context of the schema obtained from the URL",
                 status: "failed",
@@ -86,14 +69,14 @@ export function SchemaDetails() {
             }
           })
           .catch((error) => {
-            setJsonLdType({
+            setContextTuple({
               error: extractError(error),
               status: "failed",
             });
           });
       })
       .catch((error) => {
-        setSchema({
+        setSchemaTuple({
           error: extractError(error),
           status: "failed",
         });
@@ -152,8 +135,8 @@ export function SchemaDetails() {
 
   const loading =
     isAsyncTaskStarting(apiSchema) ||
-    isAsyncTaskStarting(schema) ||
-    isAsyncTaskStarting(jsonLdType);
+    isAsyncTaskStarting(schemaTuple) ||
+    isAsyncTaskStarting(contextTuple);
 
   return (
     <SiderLayoutContent
@@ -165,7 +148,7 @@ export function SchemaDetails() {
       {(() => {
         if (hasAsyncTaskFailed(apiSchema)) {
           return (
-            <Card style={{ margin: "auto", maxWidth: CARD_WIDTH }}>
+            <Card className="centered">
               <ErrorResult
                 error={[
                   "An error occurred while downloading or parsing the schema from the API:",
@@ -174,166 +157,87 @@ export function SchemaDetails() {
               />
             </Card>
           );
-        } else if (hasAsyncTaskFailed(schema)) {
+        } else if (hasAsyncTaskFailed(schemaTuple)) {
           return (
-            <Card style={{ margin: "auto", maxWidth: CARD_WIDTH }}>
-              <ErrorResult error={schemaErrorToString(schema.error)} />
+            <Card className="centered">
+              <ErrorResult error={schemaErrorToString(schemaTuple.error)} />
             </Card>
           );
-        } else if (hasAsyncTaskFailed(jsonLdType)) {
+        } else if (hasAsyncTaskFailed(contextTuple)) {
           return (
-            <Card style={{ margin: "auto", maxWidth: CARD_WIDTH }}>
-              <ErrorResult error={jsonLdTypeErrorToString(jsonLdType.error)} />
+            <Card className="centered">
+              <ErrorResult error={jsonLdTypeErrorToString(contextTuple.error)} />
             </Card>
           );
         } else if (loading) {
           return (
-            <Card style={{ margin: "auto", maxWidth: CARD_WIDTH }}>
+            <Card className="centered">
               <LoadingResult />
             </Card>
           );
         } else {
+          const { bigInt, createdAt, hash, url } = apiSchema.data;
+          const [schema, rawJsonSchema] = schemaTuple.data;
+          const [jsonLdType, rawJsonLdContext] = contextTuple.data;
+
           return (
-            <Card
-              extra={
-                <Dropdown
-                  menu={{
-                    items: [
-                      {
-                        key: "formatted",
-                        label: JSON_VIEW_LABELS["formatted"],
-                        onClick: () => {
-                          setJsonView("formatted");
-                        },
-                      },
-                      {
-                        key: "jsonLdContext",
-                        label: JSON_VIEW_LABELS["jsonLdContext"],
-                        onClick: () => {
-                          setJsonView("jsonLdContext");
-                        },
-                      },
-                      {
-                        key: "jsonSchema",
-                        label: JSON_VIEW_LABELS["jsonSchema"],
-                        onClick: () => {
-                          setJsonView("jsonSchema");
-                        },
-                      },
-                    ],
-                  }}
-                >
-                  <Button style={{ margin: "16px 0" }}>
-                    {JSON_VIEW_LABELS[jsonView]} <ChevronDownIcon />
+            <SchemaViewer
+              actions={
+                <Space size="middle">
+                  <Button
+                    icon={<CreditCardIcon />}
+                    onClick={() => {
+                      navigate(generatePath(ROUTES.issueCredential.path, { schemaID }));
+                    }}
+                    type="primary"
+                  >
+                    Issue credential
                   </Button>
-                </Dropdown>
+                </Space>
               }
-              style={{ margin: "auto", maxWidth: CARD_WIDTH }}
-              title={jsonLdType.data.name}
-            >
-              <Space direction="vertical" size="large">
-                <Card className="background-grey">
-                  <Space direction="vertical">
-                    <Typography.Text type="secondary">SCHEMA DETAILS</Typography.Text>
+              contents={
+                <Space direction="vertical">
+                  <Typography.Text type="secondary">SCHEMA DETAILS</Typography.Text>
 
-                    <Detail copyable data={apiSchema.data.bigInt} label="BigInt" />
+                  <Detail copyable data={bigInt} label="BigInt" />
 
-                    <Detail copyable data={apiSchema.data.hash} label="Hash" />
+                  <Detail copyable data={hash} label="Hash" />
 
-                    <Detail copyable data={apiSchema.data.url} label="URL" />
+                  <Detail copyable data={url} label="URL" />
 
-                    <Detail data={formatDate(apiSchema.data.createdAt, true)} label="Import date" />
+                  <Detail data={formatDate(createdAt, true)} label="Import date" />
 
-                    <Row justify="space-between">
-                      <Typography.Text type="secondary">Download</Typography.Text>
+                  <Row justify="space-between">
+                    <Typography.Text type="secondary">Download</Typography.Text>
 
-                      <Button
-                        onClick={() => {
-                          downloadJsonFromUrl({
-                            fileName: schema.data.name,
-                            url: apiSchema.data.url,
-                          })
-                            .then(() => {
-                              void message.success("Schema successfully downloaded");
-                            })
-                            .catch(() => {
-                              void message.error(
-                                "An error occurred while downloading the schema. Please try again"
-                              );
-                            });
-                        }}
-                        style={{ height: 24, padding: 0 }}
-                        type="link"
-                      >
-                        JSON Schema
-                      </Button>
-                    </Row>
-                  </Space>
-                </Card>
-
-                {(() => {
-                  switch (jsonView) {
-                    case "formatted": {
-                      return (
-                        <Card className="background-grey">
-                          <Space direction="vertical">
-                            <Typography.Text type="secondary">ATTRIBUTES</Typography.Text>
-
-                            <SchemaTree className="background-grey" schema={schema.data} />
-                          </Space>
-                        </Card>
-                      );
-                    }
-                    case "jsonLdContext": {
-                      return (
-                        <SyntaxHighlighter
-                          className="background-grey"
-                          customStyle={{
-                            borderRadius: 8,
-                            margin: 0,
-                            padding: 16,
-                          }}
-                          language="json"
-                          style={a11yLight}
-                        >
-                          {JSON.stringify(rawJsonLdContext, null, 2)}
-                        </SyntaxHighlighter>
-                      );
-                    }
-                    case "jsonSchema": {
-                      return (
-                        <SyntaxHighlighter
-                          className="background-grey"
-                          customStyle={{
-                            borderRadius: 8,
-                            margin: 0,
-                            padding: 16,
-                          }}
-                          language="json"
-                          style={a11yLight}
-                        >
-                          {JSON.stringify(rawJsonSchema, null, 2)}
-                        </SyntaxHighlighter>
-                      );
-                    }
-                  }
-                })()}
-
-                <Row justify="end">
-                  <Space size="middle">
                     <Button
-                      icon={<CreditCardIcon />}
                       onClick={() => {
-                        navigate(generatePath(ROUTES.issueCredential.path, { schemaID }));
+                        downloadJsonFromUrl({
+                          fileName: schema.name,
+                          url: url,
+                        })
+                          .then(() => {
+                            void message.success("Schema downloaded successfully.");
+                          })
+                          .catch(() => {
+                            void message.error(
+                              "An error occurred while downloading the schema. Please try again."
+                            );
+                          });
                       }}
-                      type="primary"
+                      style={{ height: 24, padding: 0 }}
+                      type="link"
                     >
-                      Issue credential
+                      JSON Schema
                     </Button>
-                  </Space>
-                </Row>
-              </Space>
-            </Card>
+                  </Row>
+                </Space>
+              }
+              jsonLdType={jsonLdType}
+              rawJsonLdContext={rawJsonLdContext}
+              rawJsonSchema={rawJsonSchema}
+              schema={schema}
+            />
           );
         }
       })()}
