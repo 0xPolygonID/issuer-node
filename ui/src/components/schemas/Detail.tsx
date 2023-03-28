97b5cf323a4d6565416106fa01132b41f20d6727
amonsosanz <amonsosanz@gmail.com>
2023-03-28 18:48:52 +0200

    Implement the schema details page (#248)
    * Fix favicon path
    
    * Improve the docs
    
    * Add dev env
    
    * Update get schemas URL
    
    * Encode credentials
    
    * Encode credentials
    
    * Fix merge issue
    
    * Update dev env defaults
    
    * Update Schema interface
    
    * Integrate import schema and get schemas API requests
    
    * Remove VITE_ISSUER_NAME from .env.example
    
    * Remove VITE_ISSUER_DID from .env.example
    
    * Remove resultImportSchema
    
    * Remove leftover import
    
    * Add a description to the Import schema screen
    
    * Change schema import notification to success
    
    * Replace unnecessary backticks with double quotes
    
    * Rename function param
    
    * Make error result description wrap lines
    
    * Remove no-unused-vars exception
    
    * Rename .env.example as .env.sample
    
    * Add Local issuer as placeholder for env var
    
    * Remove comment and render the schema type instead
    
    * Remove sample env
    
    * Implement the schema details page
    
    * Fix a bug that prevented some errors from being shown
    
    * Set copyable default as false in Detail
    
    * Show schema hash and bigint from the API
    
    * Remove leftover maxWidth prop from detail
    
    * autoFocus the import schema input
    
    * Fix merge issue
    
    * Navigate to issue credential
    
    * Rename onGetApiSchema as fetchApiSchema
    
    * Use hasAsyncTaskFailed
    
    * Use hasAsyncTaskFailed
    
    * Improve error msgs and logic
    
    * Improved DOM & style reuse.
    
    * Added important override.
    
    * DRYing up schema details & preview. (#262)
    
    * Added SchemaViewer & JSONHighlighter, integrated with schema preview & details.
    
    * Reverted test schema change.
    
    * Removed unneeded checks.
    
    * Improved view DOM.
    
    * Avoid unknown error and simplify state (#264)
    
    * Avoid unknown error and simplify state
    
    * Rename getJsonLdTypesFromUrl as getSchemaJsonLdTypes
    
    * Added card centered class
    
    ---------
    
    Co-authored-by: Chris N <chris@polygon.technology>
    
    ---------
    
    Co-authored-by: amonsosanz <amonsosanz@gmail.com>
    
    * Make the JSON required in the JSONHighlighter component
    
    * Rename error builder functions
    
    * Remove CONTENT_WIDTH constant
    
    ---------
    
    Co-authored-by: Chris N <chris@polygon.technology>
    Co-authored-by: Chris N <cneuro@users.noreply.github.com>

--- ui/src/components/schemas/Detail.tsx
+++ ui/src/components/schemas/Detail.tsx
@@ -4,14 +4,13 @@ import { ReactComponent as IconCheckMark } from "src/assets/icons/check.svg";
 import { ReactComponent as IconCopy } from "src/assets/icons/copy-01.svg";
 
 export function Detail({
-  copyable = true,
+  copyable = false,
   data,
   label,
 }: {
   copyable?: boolean;
   data: string;
   label: string;
-  maxWidth?: number;
 }) {
   return (
     <Row justify="space-between">
