fd9dd6996aa603b93bf3d6b3863e6b727175213e
Chris N <chris@polygon.technology>
2023-04-03 18:52:13 +0200

    Simplified getStrictParser.

--- ui/src/utils/types.ts
+++ ui/src/utils/types.ts
@@ -54,7 +54,8 @@ export function hasAsyncTaskFailed<Data, Error>(
 // Asserts whether 2 types are exactly the same.
 type Exact<T, U> = [T, U] extends [U, T] ? true : false;
 
-// A function that accepts 2 generic types to generate a function that accepts a parser if the types match, otherwise it raises a static error).
+// A function that accepts two types then generates a function that accepts a parser for them if the types match,
+// otherwise it produces a static error.
 export function getStrictParser<Input, Output = Input>(): <
   ParserOutput,
   ParserInput = ParserOutput
@@ -64,11 +65,11 @@ export function getStrictParser<Input, Output = Input>(): <
     ZodSchema<Output, ZodTypeDef, Input>,
     ZodSchema<ParserOutput, ZodTypeDef, ParserInput>
   > extends true
-    ? // 2. then if the output types match exactly
+    ? // 2. then if the output types match exactly,
       Exact<Required<Output>, Required<ParserOutput>> extends true
       ? // 3. then if the input types match exactly,
         Exact<Required<Input>, Required<ParserInput>> extends true
-        ? // 3. return type as ZodSchema (it is certain that I/O and parser I/O are identical)
+        ? // 3. return type as a ZodSchema parser (it is certain that I/O and parser I/O are identical).
           ZodSchema<ParserOutput, ZodTypeDef, ParserInput>
         : // 3. fail
           never
@@ -77,7 +78,5 @@ export function getStrictParser<Input, Output = Input>(): <
     : // 1. fail
       never
 ) => ZodSchema<Output, ZodTypeDef, Input> {
-  return <Output, Input = Output>(parser: unknown) =>
-    // eslint-disable-next-line no-type-assertion/no-type-assertion
-    parser as ZodSchema<Output, ZodTypeDef, Input>;
+  return (parser: ZodSchema) => parser;
 }
