2f859239c9919c7d6e8c4bc38a3546d179b7c365
javip97 <javierperezies@gmail.com>
2023-04-04 14:18:07 +0200

    fix: hotfix missing field

--- internal/repositories/claims.go
+++ internal/repositories/claims.go
@@ -767,7 +767,8 @@ func (c *claims) GetAuthClaimsForPublishing(ctx context.Context, conn db.Querier
 		identity_state,     
 		identity_states.status,
        	credential_status,
-       	core_claim
+       	core_claim,
+       	revoked
 	FROM claims
 	LEFT JOIN identity_states  ON claims.identity_state = identity_states.state
 	LEFT JOIN revocation  ON claims.rev_nonce = revocation.nonce AND claims.issuer = revocation.identifier
