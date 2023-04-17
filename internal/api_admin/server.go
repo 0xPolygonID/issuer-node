63c57d42aeec4de9d99947be79f02c40c625d8bd
Javi <javierperezies@gmail.com>
2023-04-17 12:50:08 +0200

    feat: adding agent endpoint in admin api (#314)
    * feat: adding agent endpoint in admin api
    
    * fix: solving lint

--- internal/api_admin/server.go
+++ internal/api_admin/server.go
@@ -13,6 +13,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	core "github.com/iden3/go-iden3-core"
 	"github.com/iden3/iden3comm"
+	"github.com/iden3/iden3comm/packers"
 
 	"github.com/polygonid/sh-id-platform/internal/common"
 	"github.com/polygonid/sh-id-platform/internal/config"
@@ -597,6 +598,41 @@ func (s *Server) GetLinkQRCode(ctx context.Context, request GetLinkQRCodeRequest
 	return GetLinkQRCode400JSONResponse{N400JSONResponse{
 		Message: fmt.Sprintf("error fetching the link qr code: %s", err),
 	}}, nil
+}
+
+// Agent is the controller to fetch credentials from mobile
+func (s *Server) Agent(ctx context.Context, request AgentRequestObject) (AgentResponseObject, error) {
+	if request.Body == nil || *request.Body == "" {
+		log.Debug(ctx, "agent empty request")
+		return Agent400JSONResponse{N400JSONResponse{"cannot proceed with an empty request"}}, nil
+	}
+	basicMessage, err := s.packageManager.UnpackWithType(packers.MediaTypeZKPMessage, []byte(*request.Body))
+	if err != nil {
+		log.Debug(ctx, "agent bad request", "err", err, "body", *request.Body)
+		return Agent400JSONResponse{N400JSONResponse{"cannot proceed with the given request"}}, nil
+	}
+
+	req, err := ports.NewAgentRequest(basicMessage)
+	if err != nil {
+		log.Error(ctx, "agent parsing request", err)
+		return Agent400JSONResponse{N400JSONResponse{err.Error()}}, nil
+	}
+
+	agent, err := s.claimService.Agent(ctx, req)
+	if err != nil {
+		log.Error(ctx, "agent error", err)
+		return Agent400JSONResponse{N400JSONResponse{err.Error()}}, nil
+	}
+
+	return Agent200JSONResponse{
+		Body:     agent.Body,
+		From:     agent.From,
+		Id:       agent.ID,
+		ThreadID: agent.ThreadID,
+		To:       agent.To,
+		Typ:      string(agent.Typ),
+		Type:     string(agent.Type),
+	}, nil
 }
 
 func getCredentialsFilter(ctx context.Context, userDID *string, status *GetCredentialsParamsStatus, query *string) (*ports.ClaimsFilter, error) {
