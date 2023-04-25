6db7162de53274262300fc5e28e5a45b17e9286d
Alex Monso <amonsosanz@gmail.com>
2023-04-25 16:17:55 +0200

    Make links clicable in the Details component

--- ui/src/components/shared/Detail.tsx
+++ ui/src/components/shared/Detail.tsx
@@ -1,5 +1,6 @@
 import { Row, Tag, Typography } from "antd";
 
+import { z } from "zod";
 import { ReactComponent as IconCheckMark } from "src/assets/icons/check.svg";
 import { ReactComponent as IconCopy } from "src/assets/icons/copy-01.svg";
 
@@ -26,28 +27,38 @@ export function Detail({
   text: string;
 }) {
   const value = ellipsisPosition ? text.slice(0, text.length - ellipsisPosition) : text;
+  const isUrl = z.string().url().safeParse(text).success;
+  const element = (
+    <Typography.Text
+      copyable={
+        copyable && {
+          icon: [<IconCopy key={0} />, <IconCheckMark key={1} />],
+          text,
+        }
+      }
+      ellipsis={ellipsisPosition ? { suffix: text.slice(-ellipsisPosition) } : true}
+      style={{ textAlign: "right", width: 350 }}
+    >
+      {flavor.type === "plain" ? (
+        value
+      ) : (
+        <Tag color={flavor.color} style={{ marginInlineEnd: "initial" }}>
+          {value}
+        </Tag>
+      )}
+    </Typography.Text>
+  );
+
   return (
     <Row justify="space-between">
       <Typography.Text type="secondary">{label}</Typography.Text>
-
-      <Typography.Text
-        copyable={
-          copyable && {
-            icon: [<IconCopy key={0} />, <IconCheckMark key={1} />],
-            text,
-          }
-        }
-        ellipsis={ellipsisPosition ? { suffix: text.slice(-ellipsisPosition) } : true}
-        style={{ textAlign: "right", width: 350 }}
-      >
-        {flavor.type === "plain" ? (
-          value
-        ) : (
-          <Tag color={flavor.color} style={{ marginInlineEnd: "initial" }}>
-            {value}
-          </Tag>
-        )}
-      </Typography.Text>
+      {isUrl ? (
+        <Typography.Link href={text} target="_blank">
+          {element}
+        </Typography.Link>
+      ) : (
+        element
+      )}
     </Row>
   );
 }
