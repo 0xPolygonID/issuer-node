debdc3aac85516c4d2cb5cc2992f734e7e0851b1
Alex Monso <amonsosanz@gmail.com>
2024-01-18 18:17:17 +0100

    Rename max_results as maxResults

--- ui/src/adapters/api/credentials.ts
+++ ui/src/adapters/api/credentials.ts
@@ -91,13 +91,13 @@ export async function getCredential({
 
 export async function getCredentials({
   env,
-  params: { did, max_results, page, query, status },
+  params: { did, maxResults, page, query, status },
   signal,
 }: {
   env: Env;
   params: {
     did?: string;
-    max_results?: number;
+    maxResults?: number;
     page?: number;
     query?: string;
     status?: CredentialStatus;
@@ -115,7 +115,7 @@ export async function getCredentials({
         ...(did !== undefined ? { did } : {}),
         ...(query !== undefined ? { [QUERY_SEARCH_PARAM]: query } : {}),
         ...(status !== undefined && status !== "all" ? { [STATUS_SEARCH_PARAM]: status } : {}),
-        ...(max_results !== undefined ? { max_results: max_results.toString() } : {}),
+        ...(maxResults !== undefined ? { max_results: maxResults.toString() } : {}),
         ...(page !== undefined ? { page: page.toString() } : {}),
       }),
       signal,
