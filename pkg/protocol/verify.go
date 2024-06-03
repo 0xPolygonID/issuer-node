e21f60e2f205fcde099ae1cb66acff0fd8956b3a
vbasiuk <volodymyr.basiuck@gmail.com>
2024-06-03 16:25:29 +0300

    rm check 'root doesn't exist' for no-transit state contract

--- pkg/protocol/verify.go
+++ pkg/protocol/verify.go
@@ -46,9 +46,9 @@ func authV2CircuitStateVerification(contract *abi.State, pubsignals []string) er
 	if err != nil {
 		return err
 	}
-	if (big.NewInt(0)).Cmp(globalStateInfo.CreatedAtTimestamp) == 0 {
-		return errors.Errorf("root %s doesn't exist in smart contract", globalState.String())
-	}
+	// if (big.NewInt(0)).Cmp(globalStateInfo.CreatedAtTimestamp) == 0 {
+	// 	return errors.Errorf("root %s doesn't exist in smart contract", globalState.String())
+	// }
 	if globalState.Cmp(globalStateInfo.Root) != 0 {
 		return errors.Errorf("invalid global state info in the smart contract, expected root %s, got %s", globalState.String(), globalStateInfo.Root.String())
 	}
