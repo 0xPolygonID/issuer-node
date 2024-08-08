67302cea54e523137cb72b371be745894854783c
Bartek <blech@privado.id>
2024-08-08 16:34:26 +0200

    remove commented config

--- api_tests/playwright.config.ts
+++ api_tests/playwright.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   use: {
-    // baseURL: 'https://issuer-node-api-v2-test.privado.id',//TODO get from container
     baseURL: `${process.env.ISSUER_API_URL}`,
     extraHTTPHeaders: {
       'Authorization': `Basic ${btoa(`${process.env.ISSUER_API_UI_AUTH_USER}:${process.env.ISSUER_API_UI_AUTH_PASSWORD}`)}`,
