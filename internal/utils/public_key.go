c5a2e364fd150bd0c5c8605b549f3f5f5ce3602a
martinsaporiti <martinsaporiti@gmail.com>
2024-12-16 09:07:40 -0300

    chore: move publickey to utils package

---
+++ internal/utils/public_key.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"bytes"
+
+	"github.com/iden3/go-iden3-crypto/babyjub"
+
+	"github.com/polygonid/sh-id-platform/internal/core/domain"
+)
+
+const (
+	bjjAuthSchemaURL = "https://schema.iden3.io/core/json/auth.json"
+)
+
+// PublicKey - defines the interface for public keys
+type PublicKey interface {
+	Equal([]byte) bool
+	String() string
+}
+
+type bjjPublicKey struct {
+	publicKey babyjub.PublicKey
+}
+
+// newBJJPublicKey creates a new PublicKey from a Claim
+func newBJJPublicKey(claim domain.Claim) PublicKey {
+	entry := claim.CoreClaim.Get()
+	bjjClaim := entry.RawSlotsAsInts()
+	var authCoreClaimPublicKey babyjub.PublicKey
+	authCoreClaimPublicKey.X = bjjClaim[2]
+	authCoreClaimPublicKey.Y = bjjClaim[3]
+	return &bjjPublicKey{publicKey: authCoreClaimPublicKey}
+}
+
+func (b *bjjPublicKey) Equal(pubKey []byte) bool {
+	compPubKey := b.publicKey.Compress()
+	return bytes.Equal(pubKey, compPubKey[:])
+}
+
+func (b *bjjPublicKey) String() string {
+	return "0x" + b.publicKey.String()
+}
+
+type unSupportedPublicKeyType struct{}
+
+func (u *unSupportedPublicKeyType) Equal([]byte) bool {
+	return false
+}
+
+func (u *unSupportedPublicKeyType) String() string {
+	return ""
+}
+
+// GetPublicKeyFromClaim returns the public key of the claim
+// If the schema is not supported, it returns an unSupportedPublicKeyType
+func GetPublicKeyFromClaim(c *domain.Claim) PublicKey {
+	if c.SchemaURL == bjjAuthSchemaURL {
+		return newBJJPublicKey(*c)
+	}
+	return &unSupportedPublicKeyType{}
+}
