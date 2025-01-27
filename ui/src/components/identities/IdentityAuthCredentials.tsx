93ed11f539cc3ba03ba52150136066c7c731ecb7
Oleksandr Raspopov <38541764+Alexander-frenki@users.noreply.github.com>
2025-01-27 12:38:16 +0100

    chore: add warning message when authentication credentials are revoked (#903)
    Co-authored-by: Oleksandr Raspopov <alexander.raspopov.1994@gmail.com>

--- ui/src/components/identities/IdentityAuthCredentials.tsx
+++ ui/src/components/identities/IdentityAuthCredentials.tsx
@@ -249,6 +249,11 @@ export function IdentityAuthCredentials({
       {credentialToRevoke && (
         <CredentialRevokeModal
           credential={credentialToRevoke}
+          extra={
+            <Typography.Text type="danger">
+              All BJJSignature2021 proofs signed with this key will be invalid
+            </Typography.Text>
+          }
           onClose={() => setCredentialToRevoke(undefined)}
           onRevoke={() => void fetchAuthCredentials()}
         />
